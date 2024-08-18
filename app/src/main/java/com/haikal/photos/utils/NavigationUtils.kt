ca582bc1762a0328a25f33f77c8847e102830373
Haikal <haikalharin1998@gmail.com>
2024-08-19 00:42:22 +0700

    update

--- app/src/main/java/com/haikal/photos/utils/NavigationUtils.kt
+++ app/src/main/java/com/haikal/photos/utils/NavigationUtils.kt
@@ -6,10 +6,5 @@ import com.haikal.photos.presentation.photos.PhotosActivity
 
 object NavigationUtils {
 
-    fun navigateToMovies(activity: Activity, genreId: Int?) {
-        val intent = Intent(activity, PhotosActivity::class.java)
-        intent.putExtra(PhotosActivity.KEY_GENRE_ID, genreId)
-        activity.startActivity(intent)
-    }
 
 }
